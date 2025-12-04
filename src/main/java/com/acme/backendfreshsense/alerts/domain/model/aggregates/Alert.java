1dfbbdabf089671724a525333d1e90a0adb5acb5
Manuel <u20241C134@upc.edu.pe>
2025-12-04 11:35:16 -0500

    feat: Alerts Put

--- src/main/java/com/acme/backendfreshsense/alerts/domain/model/aggregates/Alert.java
+++ src/main/java/com/acme/backendfreshsense/alerts/domain/model/aggregates/Alert.java
@@ -1,24 +1,31 @@
 package com.acme.backendfreshsense.alerts.domain.model.aggregates;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Table(name = "alert")
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Alert {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String severity;   // critical | warning | info
-    private String state;      // active | muted | resolved
     private String title;
-    private String message;
-    private String source;     // Temp | Humidity | Ethylene | Cleanliness | Oxygen
-    private String timeAgo;    // "2m ago", "1h ago", etc.
-}
 
+    private String message;
+
+    private String severity;
+
+    private String source;
+
+    private String state;
+
+    @Column(name = "time_ago")
+    private String timeAgo;
+}
